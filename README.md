d25ab932f1cb1831a5c180a99e69adfcec7638c8
dextroseSH <pheinrich11235@gmail.com>
2021-06-30 13:34:36 +0200

    Created structure and described how to add link in other packages

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Anonymizer
 
 This is the anonymizer package.
+
+## Install this package in another package
+Run ```npm link``` here. 
+
+Run ```npm link anonymizer``` where you want to link this package.
---
+++ index.js
@@ -0,0 +1,30 @@
+const PD = require("probability-distributions");
+
+exports.addNoise = (value, {typeOfDistribution, distributionParameters, valueParameters}) => {
+    /**
+     * need map for distributions -> functions of the PD-package (https://statisticsblog.com/probability-distributions/)
+     * like: 
+     * { binominal: PD.rbinom, ...}
+     */
+    
+    //add type checking
+
+    if(value && value instanceof Number || typeof value == "number"){
+        //valueParameters musst say, whether this is an int or not
+        return value + PD.rbinom(1,10,0.5)[0]
+    }
+
+    if(value && value instanceof Date){
+        //do something here
+    }
+    
+    return -2;
+}
+
+exports.generalize = () => {
+    return 3;
+}
+
+exports.suppress = () => {
+    return null;
+}
